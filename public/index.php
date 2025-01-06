a56c28a68159428d35acf11348af77027b65a2bc
Harrisong <harrygutie7@gmail.com>
2025-01-06 17:06:15 -0600

    initial commit implementing boostrap

---
+++ public/index.php
@@ -0,0 +1,26 @@
+<?php
+require '../vendor/autoload.php';
+
+?>
+
+<!DOCTYPE html>
+<html lang="es">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Sistema-de-Gestion-de-Biblioteca</title>
+    <link href="../vendor/twbs/bootstrap/dist/css/bootstrap.min.css" rel="stylesheet">
+</head>
+
+<body>
+    <main>
+        <div class="container">
+            <h1 class="text-center text-primary">¡Bootstrap está funcionando!</h1>
+            <button class="btn btn-success">Botón de prueba</button>
+        </div>
+    </main>
+    <script src="../vendor/twbs/bootstrap/dist/js/bootstrap.bundle.min.js"></script>
+</body>
+
+</html>
