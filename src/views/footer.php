685bf3c4f6c32d952b3ad7545fbbb1c1397a02d2
Harrisong <harrygutie7@gmail.com>
2025-01-21 14:00:44 -0600

    boostrap custom

--- src/views/footer.php
+++ src/views/footer.php
@@ -1,3 +1,3 @@
-<footer class="bg-secondary text-center p-3 position-fixed bottom-0 w-100">
+<footer class="bg-secondary text-center p-3 ">
     <p class="my-2 text-white">&copy;2025 Library Management System. All rights reserved SMBS.</p>
 </footer>
